0a3b1c974c4b68d69c679282231743dec1e1cf01
Satrya <margasatrya@gmail.com>
2016-09-10 10:38:07 +0700

    Update

--- uninstall.php
+++ uninstall.php
@@ -1,16 +1,10 @@
 <?php
 /**
  * Uninstall procedure for the plugin.
- *
- * @package    Scroll_Top
- * @since      0.1.0
- * @author     Satrya
- * @copyright  Copyright (c) 2014, Satrya
- * @license    http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 /* If uninstall not called from WordPress exit. */
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
 /* Delete plugin settings. */
